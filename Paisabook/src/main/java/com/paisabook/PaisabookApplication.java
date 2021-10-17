6b5dbca755d715fdfa702eee8e6db4f81afc89f4
Abhijeet Thorat <Abhijeet.kadance.96@gmail.com>
2021-10-17 12:20:33 +0530

    Created Project Structure

---
+++ Paisabook/src/main/java/com/paisabook/PaisabookApplication.java
@@ -0,0 +1,13 @@
+package com.paisabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaisabookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaisabookApplication.class, args);
+	}
+
+}
---
+++ Paisabook/src/test/java/com/paisabook/PaisabookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paisabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaisabookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
